073fdca079ba3605063f533ff80bafe92e2f719d
Andres <Andres@DESKTOP-M9II48O>
2019-08-18 15:20:45 -0500

    Add Image

--- MyLeasing.Web/Controllers/OwnersController.cs
+++ MyLeasing.Web/Controllers/OwnersController.cs
@@ -16,27 +16,30 @@ namespace MyLeasing.Web.Controllers
     [Authorize(Roles ="Manager")]
     public class OwnersController : Controller
     {
-        private readonly DataContext _datacontext;
+        private readonly DataContext _dataContext;
         private readonly IUserHelper _userHelper;
         private readonly ICombosHelper _combosHelper;
         private readonly IConverterHelper _converterHelper;
+        private readonly IImageHelper _imageHelper;
 
         public OwnersController(
             DataContext datacontext,
             IUserHelper userHelper,
             ICombosHelper  combosHelper,
-            IConverterHelper converterHelper)
+            IConverterHelper converterHelper,
+            IImageHelper imageHelper)
         {
-            _datacontext = datacontext;
+            _dataContext = datacontext;
             _userHelper = userHelper;
             _combosHelper = combosHelper;
             _converterHelper = converterHelper;
+            _imageHelper = imageHelper;
         }
 
         // GET: Owners
         public IActionResult Index()
         {
-            return View( _datacontext.Owners
+            return View( _dataContext.Owners
                 .Include(o=>o.User)
                 .Include(o=>o.Properties)
                 .Include(o=>o.Contracts));
@@ -50,7 +53,7 @@ namespace MyLeasing.Web.Controllers
                 return NotFound();
             }
 
-            var owner = await _datacontext.Owners
+            var owner = await _dataContext.Owners
                 .Include(o=>o.User)
                 .Include(o =>o.Properties)
                 .ThenInclude(p=>p.PropertyImages)
@@ -90,8 +93,8 @@ namespace MyLeasing.Web.Controllers
                         Properties=new List<Property>(),
                         User=user
                     };
-                    _datacontext.Owners.Add(owner);
-                    await _datacontext.SaveChangesAsync();
+                    _dataContext.Owners.Add(owner);
+                    await _dataContext.SaveChangesAsync();
                     return RedirectToAction("Index");
                 }
                 ModelState.TryAddModelError(string.Empty, "User with this email already exist");
@@ -130,7 +133,7 @@ namespace MyLeasing.Web.Controllers
                 return NotFound();
             }
 
-            var owner = await _datacontext.Owners.FindAsync(id);
+            var owner = await _dataContext.Owners.FindAsync(id);
             if (owner == null)
             {
                 return NotFound();
@@ -154,8 +157,8 @@ namespace MyLeasing.Web.Controllers
             {
                 try
                 {
-                    _datacontext.Update(owner);
-                    await _datacontext.SaveChangesAsync();
+                    _dataContext.Update(owner);
+                    await _dataContext.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -181,7 +184,7 @@ namespace MyLeasing.Web.Controllers
                 return NotFound();
             }
 
-            var owner = await _datacontext.Owners
+            var owner = await _dataContext.Owners
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (owner == null)
             {
@@ -196,15 +199,15 @@ namespace MyLeasing.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var owner = await _datacontext.Owners.FindAsync(id);
-            _datacontext.Owners.Remove(owner);
-            await _datacontext.SaveChangesAsync();
+            var owner = await _dataContext.Owners.FindAsync(id);
+            _dataContext.Owners.Remove(owner);
+            await _dataContext.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool OwnerExists(int id)
         {
-            return _datacontext.Owners.Any(e => e.Id == id);
+            return _dataContext.Owners.Any(e => e.Id == id);
         }
         public async Task<IActionResult> AddProperty(int? id)
         {
@@ -213,7 +216,7 @@ namespace MyLeasing.Web.Controllers
                 return NotFound();
             }
 
-            var owner = await _datacontext.Owners.FindAsync(id); 
+            var owner = await _dataContext.Owners.FindAsync(id); 
             if(owner== null)
             {
                 return NotFound();
@@ -234,8 +237,8 @@ namespace MyLeasing.Web.Controllers
             if (ModelState.IsValid)
             {
                 var property = await  _converterHelper.ToPropertyAsync(model, true);
-                _datacontext.Properties.Add(property);
-                await _datacontext.SaveChangesAsync();
+                _dataContext.Properties.Add(property);
+                await _dataContext.SaveChangesAsync();
                 return RedirectToAction($"Details/{ model.OwnerId}");
             }
             return View(model);
@@ -248,7 +251,7 @@ namespace MyLeasing.Web.Controllers
                 return NotFound();
             }
 
-            var property = await _datacontext.Properties
+            var property = await _dataContext.Properties
                 .Include(p => p.Owner)
                 .Include(p => p.PropertyType)
                 .FirstOrDefaultAsync(p => p.Id == id);
@@ -267,10 +270,81 @@ namespace MyLeasing.Web.Controllers
             if (ModelState.IsValid)
             {
                 var property = await _converterHelper.ToPropertyAsync(model, false);
-                _datacontext.Properties.Update(property);
-                await _datacontext.SaveChangesAsync();
+                _dataContext.Properties.Update(property);
+                await _dataContext.SaveChangesAsync();
                 return RedirectToAction($"Details/{ model.OwnerId}");
             }
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> DetailsProperty(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var property = await _dataContext.Properties
+                .Include(o => o.Owner)
+                .ThenInclude(o => o.User)
+                .Include(o => o.Contracts)
+                .ThenInclude(c => c.Lessee)
+                .ThenInclude(l => l.User)
+                .Include(o => o.PropertyType)
+                .Include(p => p.PropertyImages)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            return View(property);
+        }
+        public async Task<IActionResult> AddImage(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var property = await _dataContext.Properties.FindAsync(id.Value);
+            if (property == null)
+            {
+                return NotFound();
+            }
+
+            var model = new PropertyImageViewModel
+            {
+                Id = property.Id
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddImage(PropertyImageViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var path = string.Empty;
+
+                if (model.ImageFile != null)
+                {
+                    path = await _imageHelper.UploadImageAsync(model.ImageFile);
+                }
+
+                var propertyImage = new PropertyImage
+                {
+                    ImageUrl = path,
+                    Property = await _dataContext.Properties.FindAsync(model.Id)
+                };
+
+                _dataContext.PropertyImages.Add(propertyImage);
+                await _dataContext.SaveChangesAsync();
+                return RedirectToAction($"{nameof(DetailsProperty)}/{model.Id}");
+            }
+
             return View(model);
         }
 
