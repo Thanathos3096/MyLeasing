281785259f2aa8671f166181de16e105b16ea6d9
Andres <Andres@DESKTOP-M9II48O>
2019-09-26 23:55:54 -0500

    WSelt Properties Registration

--- MyLeasing.Web/Controllers/HomeController.cs
+++ MyLeasing.Web/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyLeasing.Web.Data;
+using MyLeasing.Web.Helpers;
 using MyLeasing.Web.Models;
 
 namespace MyLeasing.Web.Controllers
@@ -14,11 +16,17 @@ namespace MyLeasing.Web.Controllers
     public class HomeController : Controller
     {
         private readonly DataContext _dataContext;
+        private readonly ICombosHelper _combosHelper;
+        private readonly IConverterHelper _converterHelper;
 
         public HomeController(
-           DataContext dataContext)
+           DataContext dataContext,
+           ICombosHelper combosHelper,
+           IConverterHelper converterHelper)
         {
             _dataContext = dataContext;
+            _combosHelper = combosHelper;
+            _converterHelper = converterHelper;
         }
         public IActionResult Index()
         {
@@ -80,6 +88,60 @@ namespace MyLeasing.Web.Controllers
             return View(property);
         }
 
+        [Authorize(Roles = "Owner")]
+        public async Task<IActionResult> MyProperties()
+        {
+            var owner = await _dataContext.Owners
+                .Include(o => o.User)
+                .Include(o => o.Contracts)
+                .Include(o => o.Properties)
+                .ThenInclude(p => p.PropertyType)
+                .Include(o => o.Properties)
+                .ThenInclude(p => p.PropertyImages)
+                .FirstOrDefaultAsync(o => o.User.UserName.ToLower().Equals(User.Identity.Name.ToLower()));
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            return View(owner);
+        }
+        [Authorize(Roles = "Owner")]
+        public async Task<IActionResult> AddProperty(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var owner = await _dataContext.Owners.FindAsync(id.Value);
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            var view = new PropertyViewModel
+            {
+                OwnerId = owner.Id,
+                PropertyTypes = _combosHelper.GetComboPropertyTypes()
+            };
+
+            return View(view);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProperty(PropertyViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var property = await _converterHelper.ToPropertyAsync(model, true);
+                _dataContext.Properties.Add(property);
+                await _dataContext.SaveChangesAsync();
+                return RedirectToAction(nameof(MyProperties));
+            }
+
+            return View(model);
+        }
 
     }
 }
